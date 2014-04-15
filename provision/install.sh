4f39f9b880c07ce1ebb6533e856ef3ebe25ce50c
Jérôme Corriveau <jerome.corriveau@libeo.com>
2014-04-15 17:02:55 -0400

    Change to optimize the vm installation

--- provision/install.sh
+++ provision/install.sh
@@ -12,14 +12,14 @@ sh /vagrant/provision/update.sh
 sh /vagrant/provision/git.sh
 sh /vagrant/provision/fish.sh
 sh /vagrant/provision/httpd.sh $1
-sh /vagrant/provision/mysql.sh
+#sh /vagrant/provision/mysql.sh
 
-if [[ $? == 0 ]]; then
-	expect /vagrant/provision/mysql_configurations.exp
-fi
+#if [[ $? == 0 ]]; then
+#	expect /vagrant/provision/mysql_configurations.exp
+#fi
 
-sh /vagrant/provision/php.sh $1
-sh /vagrant/provision/composer.sh
+#sh /vagrant/provision/php.sh $1
+#sh /vagrant/provision/composer.sh
 
 echo "END"
 printf "\n"
--- provision/update.sh
+++ provision/update.sh
@@ -5,6 +5,16 @@ echo "                             UPDATING PACKAGE LIST                        
 echo "********************************************************************************"
 printf "\n"
 
+if ! rpm -qa | grep -qw epel-release; then
+	sudo rpm --import https://fedoraproject.org/static/0608B895.txt
+	sudo yum install http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm -y --quiet
+fi
+
+if ! rpm -qa | grep -qw remi-release; then
+	sudo rpm --import http://rpms.famillecollet.com/RPM-GPG-KEY-remi --quiet
+	sudo yum install http://rpms.famillecollet.com/enterprise/remi-release-6.rpm -y --quiet
+fi
+
 sudo yum check-update --quiet
 sudo yum update -y --quiet
 
